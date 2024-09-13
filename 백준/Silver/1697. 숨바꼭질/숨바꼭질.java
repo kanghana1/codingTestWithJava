64dfb177cba9133ba0db8655a95fcc297e87a228
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-09-13 12:07:31 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 18644 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] count = new int[100001];
+    static int subin;
+    static int sister;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        subin = Integer.parseInt(st.nextToken());
+        sister = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs(subin) - 1);
+    }
+
+    public static int bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        int cnt = 1;
+        count[start] = cnt; // 시작지점 1로 설정
+        while(!queue.isEmpty()) {
+            int num = queue.poll();
+
+            // 정답
+            if (num == sister) return count[num];
+
+            if (num + 1 <= 100000 && count[num + 1] == 0) {
+                queue.add(num + 1);
+                count[num + 1] = count[num] + 1;
+            }
+            if (num - 1 >= 0 && count[num - 1] == 0) {
+                queue.add(num - 1);
+                count[num - 1] = count[num] + 1;
+            }
+            if (num * 2 <= 100000 && count[num * 2] == 0) {
+                queue.add(num * 2);
+                count[num * 2] = count[num] + 1;
+            }
+        }
+        return 0;
+    }
+}
