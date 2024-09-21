c1ecdd7a03b79dab1baf05c0a1bf50e8972d83f0
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-09-21 22:10:23 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 136 ms, Memory: 18372 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] location = new int[100001];
+    static Queue<Integer> queue = new LinkedList<>();
+    static int K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        location[N] = 1;
+        go(N);
+        System.out.println(location[K] - 1);
+    }
+
+    private static void go(int now) {
+        queue.add(now);
+        while (!queue.isEmpty()) {
+            int pres = queue.poll();
+            if (pres == K) break;
+
+            if (range(pres * 2)) {
+                if (location[pres * 2] == 0) {
+                    location[pres * 2] = location[pres];
+                    queue.add(pres * 2);
+                } else {
+                    location[pres * 2] = Math.min(location[pres * 2], location[pres]);
+                }
+            }
+            if (range(pres + 1)) {
+                if (location[pres + 1] == 0) {
+                    location[pres + 1] = location[pres] + 1;
+                    queue.add(pres + 1);
+                } else {
+                  location[pres + 1] = Math.min(location[pres + 1], location[pres] + 1);
+                }
+            }
+            if (range(pres - 1)) {
+                if (location[pres - 1] == 0) {
+                    location[pres - 1] = location[pres] + 1;
+                    queue.add(pres - 1);
+                } else {
+                    location[pres - 1] = Math.min(location[pres - 1], location[pres] + 1);
+                }
+            }
+        }
+    }
+
+    private static boolean range(int idx) {
+        return idx >= 0 && idx <= 100000;
+    }
+}
