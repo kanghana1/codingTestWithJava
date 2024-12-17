58e85c7c511af70f09f05c6caa24bc46fcea3b00
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-12-17 17:31:47 +0900

    [Silver I] Title: IOIOI, Time: 236 ms, Memory: 20148 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,27 +1,26 @@
 import java.io.*;
-import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int len = Integer.parseInt(br.readLine());
-        int cnt = 0;
+        int oiCnt = 0;
+        int ans = 0;
         String str = br.readLine();
-        String goal = makeStr(N);
-        for (int i = 0 ; i <= len - (2 * N + 1) ; i++) {
-            if (str.charAt(i) == 'I') {
-                if (str.substring(i, i + (2 * N + 1)).equals(goal)) cnt++;
+
+        for (int i = 1 ; i < len - 1 ;) {
+            if (str.charAt(i) == 'O' && str.charAt(i + 1) == 'I') {
+                oiCnt++;
+                if (oiCnt == N) {
+                    if (str.charAt(i - (oiCnt * 2 - 1)) == 'I') ans++;
+                    oiCnt--;
+                }
+                i += 2;
+            } else {
+                oiCnt = 0;
+                i++;
             }
         }
-        System.out.println(cnt);
-    }
-
-    private static String makeStr(int n) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("I");
-        for (int i = 0 ; i < n ; i++) {
-            sb.append("OI");
-        }
-        return sb.toString();
+        System.out.println(ans);
     }
 }
