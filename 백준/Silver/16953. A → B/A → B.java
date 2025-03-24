cb329f062da44cdcadfab7cc25d35b5f39e8dba4
강하나 <107329874+kanghana1@users.noreply.github.com>
2025-03-24 22:43:50 +0900

    [Silver II] Title: A → B, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -3,34 +3,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-import static java.lang.Integer.*;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int init = parseInt(st.nextToken());
-        String goal = st.nextToken();
-        int cnt = 0;
-        int a = 0;
-        while (true) {
-            if (parseInt(goal) == init) break;
-            else if (parseInt(goal) < init || (goal.charAt(goal.length() - 1) != '1' && parseInt(goal) % 2 == 1) ) {
-                cnt = -1;
-                break;
-            }
-            if (goal.charAt(goal.length() - 1) == '1') {
-                goal = goal.substring(0, goal.length() - 1);
-                cnt++;
-            }
-            else if (parseInt(goal) % 2 == 0) {
-                a = parseInt(goal) / 2;
-                goal = String.valueOf(a);
-                cnt++;
+        int goal = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+        System.out.println(bfs(start, goal));
+    }
+
+    private static int bfs(int start, int goal) {
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(start, 1));
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            int val = node.val;
+            if (val < goal) return -1;
+            if (val == goal) {
+                return node.cnt;
+            } else if (val % 2 == 0) {
+                queue.add(new Node(val / 2, node.cnt + 1));
+            } else if (val % 10 == 1) {
+                queue.add(new Node((val - 1) / 10, node.cnt + 1));
+            } else {
+                return -1;
             }
         }
-        if (init == Integer.parseInt(goal)) cnt++;
-        System.out.println(cnt);
+        return -1;
+    }
+}
+
+class Node {
+    int val;
+    int cnt;
+    public Node(int v, int c) {
+        this.val = v;
+        this.cnt = c;
     }
 }
