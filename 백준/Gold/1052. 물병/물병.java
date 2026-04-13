dec4e59fca108a2938a0352197186377088873aa
강하나 <107329874+kanghana1@users.noreply.github.com>
2026-04-13 16:21:37 +0900

    [Gold V] Title: 물병, Time: 116 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+        while (true) {
+            if (Integer.bitCount(N + ans) <= K) break;
+            ans++;
+        }
+        System.out.println(ans);
+    }
+}
