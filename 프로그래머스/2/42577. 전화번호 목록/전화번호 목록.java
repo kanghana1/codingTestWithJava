552d67922aabdf33b12f311ac14836832a7f585e
강하나 <107329874+kanghana1@users.noreply.github.com>
2026-04-28 22:28:26 +0900

    [level 2] Title: 전화번호 목록, Time: 341.98 ms, Memory: 99.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+     
+        Arrays.sort(phone_book);
+        Map<String, Integer> maps = new HashMap<>();
+        
+        for (int i = 0 ; i < phone_book.length - 1 ; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
+        }
+        return true;
+    }
+}
