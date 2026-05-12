28f1ac09d022469a7c4ba691cf67773db2908373
강하나 <107329874+kanghana1@users.noreply.github.com>
2026-05-12 13:11:26 +0900

    [Passed] 최대로 겹치는 구간 (95ms, 9MB)

---
+++ codetree/trail2/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int[][] arr = new int[N][2];
+        for (int i = 0 ; i < N ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken()) + 100;
+            arr[i][1] = Integer.parseInt(st.nextToken()) + 100;
+        }
+
+
+        int ans = 0;
+        int[] overlap = new int[201];
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = arr[i][0] ; j < arr[i][1] ; j++) {
+                overlap[j]++;
+            }
+        }
+
+        for (int i = 0 ; i <= 200 ; i++) {
+            ans = Math.max(ans, overlap[i]);
+        }
+        System.out.print(ans);
+
+    }
+}
